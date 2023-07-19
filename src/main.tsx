02e8a8128642f190ea08fc707cc0e184cb58f25b
itujono <itujono@gmail.com>
2023-07-19 23:56:16 +0700

    Remove unused code.

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,7 @@
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-
-// const widgets = document.querySelectorAll(".luxor-hi-widget")!;
 
 ReactDOM.createRoot(document.getElementById("kpi-widget")!).render(
   <React.StrictMode>
