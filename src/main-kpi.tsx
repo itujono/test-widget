aa29d437053bda4df1f49d2df93ea8552aee452d
itujono <itujono@gmail.com>
2023-07-20 13:00:39 +0700

    Widget kpi build.

--- src/main-kpi.tsx
+++ src/main-kpi.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import KPIWidget from "./widget/kpi.tsx";
+import App from "./App.tsx";
 
 ReactDOM.createRoot(document.getElementById("kpi-widget")!).render(
   <React.StrictMode>
-    <KPIWidget />
+    <App />
   </React.StrictMode>
 );
