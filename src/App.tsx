babcbbdd5b0d527dc070b7211e3b8d1ee40b9b69
itujono <itujono@gmail.com>
2023-07-21 15:45:09 +0700

    Assets for all combined.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import HashpriceWidget from "./widget/hashprice";
 import KPIWidget from "./widget/kpi";
+import NetworkHashrateWidget from "./widget/network-hashrate";
 
 function App() {
   return (
-    <main>
+    <main className="grid grid-cols-3 gap-4">
       <KPIWidget />
+      <HashpriceWidget />
+      <NetworkHashrateWidget />
     </main>
   );
 }
