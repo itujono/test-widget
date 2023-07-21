467b75c7681b7395aa8f78fcf9d0c1a65f6ebe86
itujono <itujono@gmail.com>
2023-07-21 15:57:28 +0700

    Query client.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import HashpriceWidget from "./widget/hashprice";
 import KPIWidget from "./widget/kpi";
 import NetworkHashrateWidget from "./widget/network-hashrate";
 
+const client = new QueryClient();
+
 function App() {
   return (
-    <main className="grid grid-cols-3 gap-4 text-clip">
-      <KPIWidget />
-      <HashpriceWidget />
-      <NetworkHashrateWidget />
-    </main>
+    <QueryClientProvider client={client}>
+      <main className="grid grid-cols-3 gap-4 text-clip">
+        <KPIWidget />
+        <HashpriceWidget />
+        <NetworkHashrateWidget />
+      </main>
+    </QueryClientProvider>
   );
 }
 
